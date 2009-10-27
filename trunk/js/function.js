e646dd40b414a0283b70cd983ec7caa6ceb97d97
rodrigopvz <rodrigopvz@e7014918-bdd1-11de-ac5a-05e7513655e2>
2009-10-27 17:57:02 +0000

    Blankito26-10

---
+++ trunk/js/function.js
@@ -0,0 +1,23 @@
+/*function muestraRut()
+{
+    var nombre = $('#nombre').val();
+      $('#rut').load(base_url+"index.php/cuentas/welcome/buscaRut/"+nombre);
+}*/
+
+function muestraRut()
+{
+    $.ajax({
+        cache:false,
+        type:"POST",
+        data:$('#userForm').serialize(),
+        url:base_url+"index.php/cuentas/welcome/buscaRut1",
+        success: function(htmlresponse,data)
+        {
+            $('#rut').html(htmlresponse);
+        }
+    });
+}
+function addField()
+{
+    
+}

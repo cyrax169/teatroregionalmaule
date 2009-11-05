fe7411bf6c2edacdfb8df091e0733a456cd9e298
cyrax169 <cyrax169@e7014918-bdd1-11de-ac5a-05e7513655e2>
2009-11-05 03:36:34 +0000

    Ahí está el menú lindi, hay que cambiar los enlaces hacia las otras vistas

--- trunk/teatro_app/views/base/content.php
+++ trunk/teatro_app/views/base/content.php
@@ -1,16 +1,31 @@
 <div id="content">
         <div id="colOne">
                 <div id="menu">
+                        <ul>
+                                <li>Gestión de Usuarios</li>
+                                <blockquote> <li><a href="#" accesskey="2" title="">Crear Trabajador</a></li></blockquote>
+                                <blockquote> <li><a href="#" accesskey="3" title="">Eliminar Trabajador</a></li></blockquote>
+                                <blockquote> <li><a href="#" accesskey="4" title="">Buscar Trabajador</a></li></blockquote>
+                                <br>
+                                <li>Impresiones</li>
+                                <blockquote> <li><a href="#" accesskey="6" title="">Planilla de Remuneraciones</a></li></blockquote>
+                                <blockquote> <li><a href="#" accesskey="7" title="">Liquidación de Sueldo</a></li></blockquote>
+                                <br>
+                                <li>Gestión de Usuarios</li>
+                                <blockquote> <li><a href="#" accesskey="9" title="">Crear Administrador</a></li></blockquote>
+                                <blockquote> <li><a href="#" accesskey="10" title="">Modificar Administrador</a></li></blockquote>
+                                <blockquote> <li><a href="#" accesskey="11" title="">Eliminar Administrador</a></li></blockquote>
                         <ul
                             <li><a href="index.php/welcome/inicio" accesskey="1" title="">Inicio</a></li>
                             <li><a href="#" accesskey="5" title="">Contáctame</a></li>
+
                         </ul>
                 </div>
         </div>
         <div id="colTwo">
                 <div class="post">
                         <h2>Sistema de Remuneraciones. </h2>
-
+                        
 
 
 			</div>

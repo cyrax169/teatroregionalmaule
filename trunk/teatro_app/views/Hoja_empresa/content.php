70a99173038b99f3c6e755f35b58c6b42553c57e
rodrigopvz <rodrigopvz@e7014918-bdd1-11de-ac5a-05e7513655e2>
2009-11-05 15:53:48 +0000

    - Se crearon als vistas de Buscar y de la hoja de vida

---
+++ trunk/teatro_app/views/Hoja_empresa/content.php
@@ -0,0 +1,43 @@
+<table width="764" border="0" align="center">
+
+  <tr>
+    <td width="194" height="27">Razon social </td>
+    <td width="560"><input type="text" name="textfield2" /></td>
+  </tr>
+  <tr>
+    <td>Rut</td>
+    <td><input type="text" name="textfield22" /></td>
+  </tr>
+
+  <tr>
+    <td>Direcci&oacute;n</td>
+    <td><input type="text" name="textfield24" /></td>
+  </tr>
+  <tr>
+    <td>Caja de compensaci&oacute;n </td>
+    <td><p>
+        <input name="radiobutton" type="radio" value="radiobutton" />
+        Si
+        <input name="radiobutton" type="radio" value="radiobutton" />
+        No
+    (SI = 6.4 % fonasa ; NO = 7% ) </p>
+    <p>
+      <input type="text" name="textfield242" />
+    (si es si sale este cuadrado para poder colocar el nombre de la caja) </p></td>
+  </tr>
+  <tr>
+    <td>Aporte Patronal </td>
+    <td><table width="200" border="0">
+      <tr>
+        <th scope="col"><input name="textfield2242" type="text" value="Mutual" /></th>
+        <th scope="col"><input name="textfield22422" type="text" value="Monto (%)" /></th>
+      </tr>
+      <tr>
+        <td><input name="textfield22423" type="text" value="IST" /></td>
+        <td><input name="textfield22424" type="text" value="Monto (%)" /></td>
+      </tr>
+    </table>
+      <p>(actualizar una vez al a&ntilde;o) </p>
+    </td>
+  </tr>
+</table>
--- trunk/teatro_app/views/base/content.php
+++ trunk/teatro_app/views/base/content.php
@@ -1,34 +1,23 @@
 <div id="content">
-        <div id="colOne">
-                <div id="menu">
-                        <ul>
-                                <li>Gestión de Usuarios</li>
-                                <blockquote> <li><a href="#" accesskey="2" title="">Crear Trabajador</a></li></blockquote>
-                                <blockquote> <li><a href="#" accesskey="3" title="">Eliminar Trabajador</a></li></blockquote>
-                                <blockquote> <li><a href="#" accesskey="4" title="">Buscar Trabajador</a></li></blockquote>
-                                <br>
-                                <li>Impresiones</li>
-                                <blockquote> <li><a href="#" accesskey="6" title="">Planilla de Remuneraciones</a></li></blockquote>
-                                <blockquote> <li><a href="#" accesskey="7" title="">Liquidación de Sueldo</a></li></blockquote>
-                                <br>
-                                <li>Gestión de Usuarios</li>
-                                <blockquote> <li><a href="#" accesskey="9" title="">Crear Administrador</a></li></blockquote>
-                                <blockquote> <li><a href="#" accesskey="10" title="">Modificar Administrador</a></li></blockquote>
-                                <blockquote> <li><a href="#" accesskey="11" title="">Eliminar Administrador</a></li></blockquote>
-                        <ul
-                            <li><a href="index.php/welcome/inicio" accesskey="1" title="">Inicio</a></li>
-                            <li><a href="#" accesskey="5" title="">Contáctame</a></li>
-
-                        </ul>
+    <div id="colOne">
+        <div id="menu">
+            <ul>
+                <li>Gestión de Usuarios</li>
+                <blockquote> <li><a href="index.php/welcome/Vida" accesskey="2" title="">Crear Trabajador</a></li></blockquote>
+                <blockquote> <li><a href="index.php/welcome/Buscar" accesskey="3" title="">Eliminar Trabajador</a></li></blockquote>
+                <blockquote> <li><a href="index.php/welcome/Buscar" accesskey="4" title="">Buscar Trabajador</a></li></blockquote>
+                <li>Impresiones</li>
+                <blockquote> <li><a href="#" accesskey="6" title="">Planilla de Remuneraciones</a></li></blockquote>
+                <blockquote> <li><a href="#" accesskey="7" title="">Liquidación de Sueldo</a></li></blockquote>
+                <li>Gestión de Usuarios</li>
+                <blockquote> <li><a href="#" accesskey="9" title="">Crear Administrador</a></li></blockquote>
+                <blockquote> <li><a href="#" accesskey="10" title="">Modificar Administrador</a></li></blockquote>
+                <blockquote> <li><a href="#" accesskey="11" title="">Eliminar Administrador</a></li></blockquote>
                 </div>
         </div>
-        <div id="colTwo">
-                <div class="post">
-                        <h2>Sistema de Remuneraciones. </h2>
-                        
-
-
-			</div>
-		</div>
-
+    <div id="colTwo">
+            <div class="post" align="center">
+                <h2>Sistema de Remuneraciones. </h2>
+           </div>
+    </div>
 </div>

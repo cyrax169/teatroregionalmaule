5c50c381d2316f7d3de76e833c65a946ceeff2a4
aKa.NasTy <aKa.NasTy@e7014918-bdd1-11de-ac5a-05e7513655e2>
2009-11-11 01:36:37 +0000

    =)

--- trunk/teatro_app/views/Hoja_empresa/content.php
+++ trunk/teatro_app/views/Hoja_empresa/content.php
@@ -1,7 +1,7 @@
 <div id="content">
     <div id="colOne">
         <div id="menu">
-            <ul>
+           <ul>
                 <br><br><br>
                 <li>Gestión de Usuarios</li>
                 <blockquote> <li><a href="../../index.php/welcome/Vida" accesskey="2" title="">Crear Trabajador</a></li></blockquote>
@@ -12,9 +12,9 @@
                 <blockquote> <li><a href="#" accesskey="7" title="">Liquidación de Sueldo</a></li></blockquote>
                 <li>Gestión de Usuarios</li>
                 <blockquote> <li><a href="#" accesskey="9" title="">Crear Administrador</a></li></blockquote>
-                <blockquote> <li><a href="#" accesskey="10" title="">Modificar Administrador</a></li></blockquote>
-                <blockquote> <li><a href="#" accesskey="11" title="">Eliminar Administrador</a></li></blockquote>
-                 <blockquote> <li><a href="../../index.php/welcome/Empresa" accesskey="12" tittle="">Datos Empresa</a></li></blockquote>
+                <blockquote> <li><a href="../../index.php/welcome/Buscar_Admin" accesskey="10" title="">Modificar Administrador</a></li></blockquote>
+                <blockquote> <li><a href="../../index.php/welcome/Buscar_Admin" accesskey="11" title="">Eliminar Administrador</a></li></blockquote>
+                <blockquote> <li><a href="../../index.php/welcome/Empresa" accesskey="12" tittle="">Datos Empresa</a></li></blockquote>
                 </div>
         </div>
     <div id="colTwo">

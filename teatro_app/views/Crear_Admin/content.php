f8b2d2a9e45248aff7e606760952d99a97bf54d8
aKa.NasTy <aKa.NasTy@e7014918-bdd1-11de-ac5a-05e7513655e2>
2009-11-11 03:47:52 +0000

    se linkea  la vista liquidacion de sueldo ( creada por la chica :p )

--- teatro_app/views/Crear_Admin/content.php
+++ teatro_app/views/Crear_Admin/content.php
@@ -9,7 +9,7 @@
                 <blockquote> <li><a href="../../index.php/welcome/Buscar" accesskey="4" title="">Buscar Trabajador</a></li></blockquote>
                 <li>Impresiones</li>
                 <blockquote> <li><a href="#" accesskey="6" title="">Planilla de Remuneraciones</a></li></blockquote>
-                <blockquote> <li><a href="#" accesskey="7" title="">Liquidación de Sueldo</a></li></blockquote>
+                <blockquote> <li><a href="../../index.php/welcome/Liquidacion" accesskey="7" title="">Liquidación de Sueldo</a></li></blockquote>
                 <li>Gestión de Usuarios</li>
                 <blockquote> <li><a href="../../index.php/welcome/Crear_Admin" accesskey="9" title="">Crear Administrador</a></li></blockquote>
                 <blockquote> <li><a href="../../index.php/welcome/Buscar_Admin" accesskey="10" title="">Modificar Administrador</a></li></blockquote>
--- teatro_app/views/Liquidacion/content.php
+++ teatro_app/views/Liquidacion/content.php
@@ -4,17 +4,17 @@
             <ul>
                 <br><br><br>
                 <li>Gestión de Usuarios</li>
-                <blockquote> <li><a href="index.php/welcome/Vida" accesskey="2" title="">Crear Trabajador</a></li></blockquote>
-                <blockquote> <li><a href="index.php/welcome/Buscar" accesskey="3" title="">Eliminar Trabajador</a></li></blockquote>
-                <blockquote> <li><a href="index.php/welcome/Buscar" accesskey="4" title="">Buscar Trabajador</a></li></blockquote>
+                <blockquote> <li><a href="../../index.php/welcome/Vida" accesskey="2" title="">Crear Trabajador</a></li></blockquote>
+                <blockquote> <li><a href="../../index.php/welcome/Buscar" accesskey="3" title="">Eliminar Trabajador</a></li></blockquote>
+                <blockquote> <li><a href="../../index.php/welcome/Buscar" accesskey="4" title="">Buscar Trabajador</a></li></blockquote>
                 <li>Impresiones</li>
                 <blockquote> <li><a href="#" accesskey="6" title="">Planilla de Remuneraciones</a></li></blockquote>
-                <blockquote> <li><a href="#" accesskey="7" title="">Liquidación de Sueldo</a></li></blockquote>
+                <blockquote> <li><a href="../../index.php/welcome/Liquidacion" accesskey="7" title="">Liquidación de Sueldo</a></li></blockquote>
                 <li>Gestión de Usuarios</li>
-                <blockquote> <li><a href="#" accesskey="9" title="">Crear Administrador</a></li></blockquote>
-                <blockquote> <li><a href="#" accesskey="10" title="">Modificar Administrador</a></li></blockquote>
-                <blockquote> <li><a href="#" accesskey="11" title="">Eliminar Administrador</a></li></blockquote>
-                <blockquote> <li><a href="index.php/welcome/Empresa" accesskey="12" tittle="">Datos Empresa</a></li></blockquote>
+                <blockquote> <li><a href="../../index.php/welcome/Crear_Admin" accesskey="9" title="">Crear Administrador</a></li></blockquote>
+                <blockquote> <li><a href="../../index.php/welcome/Buscar_Admin" accesskey="10" title="">Modificar Administrador</a></li></blockquote>
+                <blockquote> <li><a href="../../index.php/welcome/Buscar_Admin" accesskey="11" title="">Eliminar Administrador</a></li></blockquote>
+                <blockquote> <li><a href="../../index.php/welcome/Empresa" accesskey="12" tittle="">Datos Empresa</a></li></blockquote>
                 </div>
         </div>
     <div id="colTwo">

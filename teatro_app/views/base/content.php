5221af54fc6ef3d40aad753b88d17a1644451b2d
aKa.NasTy <aKa.NasTy@e7014918-bdd1-11de-ac5a-05e7513655e2>
2009-11-11 03:29:55 +0000

    Se crearon las vistas: base, crear, modificar, y eliminar administrador

--- teatro_app/views/base/content.php
+++ teatro_app/views/base/content.php
@@ -11,15 +11,38 @@
                 <blockquote> <li><a href="#" accesskey="6" title="">Planilla de Remuneraciones</a></li></blockquote>
                 <blockquote> <li><a href="#" accesskey="7" title="">Liquidación de Sueldo</a></li></blockquote>
                 <li>Gestión de Usuarios</li>
-                <blockquote> <li><a href="#" accesskey="9" title="">Crear Administrador</a></li></blockquote>
+                <blockquote> <li><a href="index.php/welcome/Crear_Admin" accesskey="9" title="">Crear Administrador</a></li></blockquote>
                 <blockquote> <li><a href="index.php/welcome/Buscar_Admin" accesskey="10" title="">Modificar Administrador</a></li></blockquote>
                 <blockquote> <li><a href="index.php/welcome/Buscar_Admin" accesskey="11" title="">Eliminar Administrador</a></li></blockquote>
                 <blockquote> <li><a href="index.php/welcome/Empresa" accesskey="12" tittle="">Datos Empresa</a></li></blockquote>
                 </div>
         </div>
-    <div id="colTwo">
+       <div id="colTwo">
             <div class="post" align="center">
-                <h2>Sistema de Remuneraciones. </h2>
-           </div>
+                <h2>SISTEMA DE REMUNERACIONES</h2>
+            </div>
+
+        <br><br><br><br>
+        <table align="center">
+                    <thead>
+                        <tr>
+                            <td>LOGIN</td>
+                            <td><input type="text" name="login" value="" size="29"/></td>
+                        </tr>
+                        <tr>
+                            <td>PASSWORD</td>
+                            <td><input type="password" name="PASSWORD" value="" size="29" /></td>
+                        </tr>
+                 </tbody>
+                </table>
+         <br><br><br><br><br>
+        <div align="center"><input type="submit" name="button" id="button" value="Aceptar"><div/>
+       <br><br><br><br><br><br><br><br><br><br><br><br><br>
+            <!--<ul>
+                            <li><a href="index.php/welcome/Vida" accesskey="1" title="">Hoja de Vida</a></li>
+                            <li><a href="index.php/welcome/Empresa" accesskey="1" title="">Hoja de Empresa</a></li>
+                            <li><a href="#" accesskey="5" title="">Contáctame</a></li>
+
+                        </ul>  -->
     </div>
 </div>

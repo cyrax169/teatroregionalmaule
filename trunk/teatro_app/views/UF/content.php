4c72993ff17c1243a119d7652f0e7761e4040087
aKa.NasTy <aKa.NasTy@e7014918-bdd1-11de-ac5a-05e7513655e2>
2009-11-19 17:47:15 +0000

    se crea la ventana UF

---
+++ trunk/teatro_app/views/UF/content.php
@@ -0,0 +1,34 @@
+<div id="content">
+    <!--div id="lateral">
+        <ul>
+        <br><br><br>
+        <li>Gestión de Usuarios</li>
+        <blockquote>
+            <li><a href="index.php/welcome/Vida" accesskey="2" title="">Crear Trabajador</a></li>
+            <li><a href="index.php/welcome/Buscar" accesskey="3" title="">Eliminar Trabajador</a></li>
+            <li><a href="index.php/welcome/Buscar" accesskey="4" title="">Buscar Trabajador</a></li>
+        </blockquote>
+            <li>Impresiones</li>
+        <blockquote>
+            <li><a href="#" accesskey="6" title="">Planilla de Remuneraciones</a></li>
+            <li><a href="index.php/welcome/Liquidacion" accesskey="7" title="">Liquidación de Sueldo</a></li>
+        </blockquote>
+            <li>Gestión de Usuarios</li>
+        <blockquote>
+            <li><a href="index.php/welcome/Crear_Admin" accesskey="9" title="">Crear Administrador</a></li>
+            <li><a href="index.php/welcome/Buscar_Admin" accesskey="10" title="">Modificar Administrador</a></li>
+            <li><a href="index.php/welcome/Buscar_Admin" accesskey="11" title="">Eliminar Administrador</a></li>
+            <li><a href="index.php/welcome/Empresa" accesskey="12" title="">Datos Empresa</a></li>
+        </blockquote>
+    </div-->
+    <form name="frm" method="post" action="">
+        <fieldset id="form">
+            <ol>
+                    <label>Ingrese UF</label>
+                    <input align="left" type="text" name="nombre" id="uf"/>
+            </ol>
+                <p align="center"><input type="submit" name="submit" class="btn" value="Enviar" /></p>
+        </fieldset>
+    </form>
+</div>
+<!--Fin Header-->

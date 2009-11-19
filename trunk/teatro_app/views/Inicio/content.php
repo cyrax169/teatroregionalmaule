464c80e9cac76724728ed4629bcbac26e34add66
rodrigopvz <rodrigopvz@e7014918-bdd1-11de-ac5a-05e7513655e2>
2009-11-19 15:48:24 +0000

    - Vistas del Login

--- trunk/teatro_app/views/Inicio/content.php
+++ trunk/teatro_app/views/Inicio/content.php
@@ -1,7 +1,32 @@
-<?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-echo anchor('usuario/logout','salir del sistema');
-?>
+<div id="content">
+    <div id="lateral">
+        <ul>
+        <br><br><br>
+        <li>Gestión de Usuarios</li>
+        <blockquote>
+            <li><a href="../../index.php/welcome/Vida" accesskey="2" title="">Crear Trabajador</a></li>
+            <li><a href="../../index.php/welcome/Buscar" accesskey="3" title="">Eliminar Trabajador</a></li>
+            <li><a href="../../index.php/welcome/Buscar" accesskey="4" title="">Buscar Trabajador</a></li>
+        </blockquote>
+            <li>Impresiones</li>
+        <blockquote>
+            <li><a href="#" accesskey="6" title="">Planilla de Remuneraciones</a></li>
+            <li><a href="../../index.php/welcome/Liquidacion" accesskey="7" title="">Liquidación de Sueldo</a></li>
+        </blockquote>
+            <li>Gestión de Usuarios</li>
+        <blockquote>
+            <li><a href="../../index.php/welcome/Crear_Admin" accesskey="9" title="">Crear Administrador</a></li>
+            <li><a href="../../index.php/welcome/Buscar_Admin" accesskey="10" title="">Modificar Administrador</a></li>
+            <li><a href="../../index.php/welcome/Buscar_Admin" accesskey="11" title="">Eliminar Administrador</a></li>
+            <li><a href="../../index.php/welcome/Empresa" accesskey="12" tittle="">Datos Empresa</a></li>
+        </blockquote>
+    </div>
+    <div id="principal">
+        <div class="post" align="center">
+            <h2>BIENVENIDOS</h2>
+        </div>
+        <br><br><br><br><br><br><br>
+        
+        <br><br><br><br><br><br><br><br><br><br><br><br>
+   </div>
+</div>

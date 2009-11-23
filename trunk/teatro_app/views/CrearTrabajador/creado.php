fb78aba98b871dbd18172762bae616c9ca8e8833
aKa.NasTy <aKa.NasTy@e7014918-bdd1-11de-ac5a-05e7513655e2>
2009-11-23 19:27:01 +0000

    Se crea Vista CrearTrabajador, se insertan los datos en la bd, se modifican cosas minimas en cada vista ademas de agregar campos y eliminar tablas de la bd ... que ta la pura caga con eso xD!

---
+++ trunk/teatro_app/views/CrearTrabajador/creado.php
@@ -0,0 +1,51 @@
+<div id="content">
+    <div id="lateral">
+        <ul>
+        <br><br><br>
+            <li>Hoja de Empresa</li>
+        <blockquote>
+            <li><a href="../../index.php/welcome/Empresa" accesskey="12" title="">Datos Empresa</a></li>
+        </blockquote>
+        <li>Gestión de Usuarios</li>
+        <blockquote>
+            <li><a href="../../index.php/welcome/Vida" accesskey="2" title="">Crear Trabajador</a></li>
+            <li><a href="../../index.php/welcome/Buscar" accesskey="3" title="">Eliminar Trabajador</a></li>
+            <li><a href="../../index.php/welcome/Buscar" accesskey="4" title="">Buscar Trabajador</a></li>
+        </blockquote>
+            <li>Impresiones</li>
+        <blockquote>
+            <li><a href="../../index.php/welcome/planilla" accesskey="6" title="">Planilla de Remuneraciones</a></li>
+            <li><a href="../../index.php/welcome/Liquidacion" accesskey="7" title="">Liquidación de Sueldo</a></li>
+        </blockquote>
+            <li>Gestión de Usuarios</li>
+        <blockquote>
+            <li><a href="../../index.php/welcome/Crear_Admin" accesskey="9" title="">Crear Administrador</a></li>
+            <li><a href="../../index.php/welcome/Modificar_Admin" accesskey="10" title="">Modificar Administrador</a></li>
+            <li><a href="../../index.php/welcome/Eliminar_Admin" accesskey="11" title="">Eliminar Administrador</a></li>
+        </blockquote>
+             <li>Tablas</li>
+        <blockquote>
+            <li><a href="../../index.php/welcome/tablaIUT" accesskey="12" title="">Impuesto Único Tributario</a></li>
+            <li><a href="#" accesskey="12" title="">Tramos Fonasa</a></li>
+        </blockquote>
+    </div>
+    <div id="principal">
+        <div class="post" align="center">
+            <h2>TRABAJADOR CREADO</h2>
+            <div align="right" id="form">
+                <form name="frm" method="post" action="<?=base_url()?>index.php/usuario/logout">
+                    <input class="btn" type="submit" name="Salir" value="Salir"/>
+                </form>
+            </div>
+        </div>
+        <br><br><br><br><br><br>
+        <td width="100" align="right">Trabajador creado con exito</td>
+        <br><br><br><br><br><br>
+        <div id="form" align="center">
+            <form name="frm" method="post" action="../welcome/CrearTrabajador">
+                <input class="btn" type="submit" name="Continuar" value="Continuar"/>
+            </form>
+        </div>
+        <br><br><br><br><br><br><br><br>
+    </div>
+</div>

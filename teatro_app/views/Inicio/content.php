235cb4731c2cb9b81483adc53c33db518c9a60bd
claudiadela7 <claudiadela7@e7014918-bdd1-11de-ac5a-05e7513655e2>
2009-11-25 02:18:48 +0000

    tramo fonasa por Tramos de asignaciones familiares

--- teatro_app/views/Inicio/content.php
+++ teatro_app/views/Inicio/content.php
@@ -20,30 +20,25 @@
             <li>Gestión de Usuarios</li>
         <blockquote>
             <li><a href="../../index.php/welcome/Crear_Admin" accesskey="9" title="">Crear Administrador</a></li>
-            <li><a href="../../index.php/welcome/Modificar_Admin" accesskey="10" title="">Modificar Administrador</a></li>
-            <li><a href="../../index.php/welcome/Eliminar_Admin" accesskey="11" title="">Eliminar Administrador</a></li>
+            <li><a href="../../index.php/welcome/Buscar_Admin" accesskey="10" title="">Modificar Administrador</a></li>
+            <li><a href="../../index.php/welcome/Buscar_Admin" accesskey="11" title="">Eliminar Administrador</a></li>
         </blockquote>
              <li>Tablas</li>
         <blockquote>
             <li><a href="../../index.php/welcome/tablaIUT" accesskey="12" title="">Impuesto Único Tributario</a></li>
-            <li><a href="#" accesskey="12" title="">Tramos Fonasa</a></li>
+            <li><a href="#" accesskey="12" title="">Tramos de asignaciones familiares</a></li>
         </blockquote>
     </div>
     <div id="principal">
         <div class="post" align="center">
             <h2>BIENVENIDOS</h2>
-            <div align="right" id="form">
-                <form name="frm" method="post" action="<?=base_url()?>index.php/usuario/logout">
-                    <input class="btn" type="submit" name="Salir" value="Salir"/>
-                </form>
-            </div>
         </div>
-        <br><br><br><br><br><br><br><br><br>   
-        <script  type="" language="JavaScript" for="window"
-            event="onload()">window.open
-            ("../../index.php/welcome/UF", "Valor UF",
-            "scrollbars=no,resizable=yes,height=230,width=380")
-        </script>
+        <br><br><br><br><br><br><br><br><br>
+        <script language="JavaScript" for="window"
+        event="onload()">window.open
+        ("../../index.php/welcome/UF", "Valor UF",
+        "scrollbars=no,resizable=yes,height=230,width=380")
+  </script>
 
         <br><br><br><br><br><br>
         <br><br><br><br><br><br><br><br><br><br><br><br>

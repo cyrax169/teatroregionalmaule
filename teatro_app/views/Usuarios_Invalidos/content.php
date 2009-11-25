1363261aa26c45ebb5d9002197f6ea1417104159
aKa.NasTy <aKa.NasTy@e7014918-bdd1-11de-ac5a-05e7513655e2>
2009-11-25 20:35:59 +0000

    Hice mi primera vista!!! es pa cuando ingresan datos invalidos en el login

---
+++ teatro_app/views/Usuarios_Invalidos/content.php
@@ -0,0 +1,17 @@
+<div id="content">
+   <form name="frm" method="post" action="<?=base_url()?>index.php/usuario/index">
+        <fieldset id="form">
+              <legend>Sistema de Remuneraciones</legend>
+            <ol>
+                <li>
+                    <label>Datos No Validos</label>
+                </li>
+            </ol>
+                <p align="center"><input type="submit" name="submit" class="btn" value="Volver" /></p>
+        </fieldset>
+   </form>
+</div>
+       
+    
+
+<!--Fin Header-->

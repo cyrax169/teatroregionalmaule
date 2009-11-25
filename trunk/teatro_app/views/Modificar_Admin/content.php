f0d08efb7543255dc9a418959afc65ef6fd304bd
claudiadela7 <claudiadela7@e7014918-bdd1-11de-ac5a-05e7513655e2>
2009-11-25 19:24:41 +0000

    un solo header

--- trunk/teatro_app/views/Modificar_Admin/content.php
+++ trunk/teatro_app/views/Modificar_Admin/content.php
@@ -1,34 +1,3 @@
-<div id="content">
-    <div id="lateral">
-        <ul>
-        <br><br><br>
-            <li>Hoja de Empresa</li>
-        <blockquote>
-            <li><a href="../../index.php/welcome/Empresa" accesskey="12" title="">Datos Empresa</a></li>
-        </blockquote>
-        <li>Gestión de Usuarios</li>
-        <blockquote>
-            <li><a href="../../index.php/welcome/CrearTrabajador" accesskey="2" title="">Crear Trabajador</a></li>
-            <li><a href="../../index.php/welcome/Buscar" accesskey="4" title="">Modificar Trabajador</a></li>
-            <li><a href="../../index.php/welcome/Buscar" accesskey="3" title="">Eliminar Trabajador</a></li>
-        </blockquote>
-            <li>Impresiones</li>
-        <blockquote>
-            <li><a href="../../index.php/welcome/planilla" accesskey="6" title="">Planilla de Remuneraciones</a></li>
-            <li><a href="../../index.php/welcome/Liquidacion" accesskey="7" title="">Liquidación de Sueldo</a></li>
-        </blockquote>
-            <li>Gestión de Usuarios</li>
-        <blockquote>
-            <li><a href="../../index.php/welcome/Crear_Admin" accesskey="9" title="">Crear Administrador</a></li>
-            <li><a href="../../index.php/welcome/Modificar_Admin" accesskey="10" title="">Modificar Administrador</a></li>
-            <li><a href="../../index.php/welcome/Eliminar_Admin" accesskey="11" title="">Eliminar Administrador</a></li>
-        </blockquote>
-             <li>Tablas</li>
-        <blockquote>
-            <li><a href="../../index.php/welcome/tablaIUT" accesskey="12" title="">Impuesto Único Tributario</a></li>
-            <li><a href="#" accesskey="12" title="">Tramos de asignaciones familiares</a></li>
-        </blockquote>
-    </div>
     <div id="principal">
         <div class="post" align="center">
             <h2>MODIFICAR ADMINISTRADOR</h2>

9622304ba7fb17e6476695a875534b332dc3ddcc
claudiadela7 <claudiadela7@e7014918-bdd1-11de-ac5a-05e7513655e2>
2009-12-11 21:23:29 +0000

    se creo la tabla UF para almacenar las UF's diarias

--- teatro_app/views/UF/content.php
+++ teatro_app/views/UF/content.php
@@ -2,8 +2,8 @@
     <form name="frm" method="post" action="<?=base_url()?>index.php/welcome/ActualizaUF">
         <fieldset id="form">
             <ol>
-                    <label>Ingrese UF</label>
-                    <input align="left" type="text" name="uf" id="uf"/>
+                <label>Ingrese UF</label>
+                <input align="left" type="text" name="uf" id="uf"/>
             </ol>
                 <p align="center"><input type="submit" name="submit" class="btn" value="Enviar" /></p>
         </fieldset>

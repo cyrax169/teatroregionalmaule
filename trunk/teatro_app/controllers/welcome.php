6855276769de40f22f648e1799422988cfd04a95
rodrigopvz <rodrigopvz@e7014918-bdd1-11de-ac5a-05e7513655e2>
2009-12-20 16:31:46 +0000

    Se puede generar PDF!, ingresando por url directamente a welcome/genera_pdf, es solo un ejemplo muy claro, con creación de tablas, títulos, hora y fecha, y numeración de páginas, espero lo vean al menos grgr!!

--- trunk/teatro_app/controllers/welcome.php
+++ trunk/teatro_app/controllers/welcome.php
@@ -8,7 +8,37 @@ class Welcome extends Controller {
                 $this->load->model('varios_model');
                 $this->load->helper(array('form', 'url'));
 		$this->load->library('form_validation');
+                $this->load->library('cezpdf');
+                $this->load->helper('pdf_helper');
 	}
+        public function genera_pdf()
+        {
+            /*load library cezpdf*/
+            prep_pdf();
+            $this->cezpdf->ezText('<b>Cliente Nº:</b> 1');
+            $this->cezpdf->ezText('<b>Cliente:</b> Rodrigo Pavez Madariaga');
+            $this->cezpdf->ezText('<b>Tienda:</b>  La Polar');
+            $this->cezpdf->ezText('<b>Fecha y hora de impresión:</b> '.date('d/m/Y').', '.date('H:i').' hrs.');
+            $this->cezpdf->ezText('');
+            $db_data[] = array('eye' => 'O.D.','ESF' => '+9.75','CIL' => '-1.25','EJE' => '3','ADD' => '+2.50','REF' => 'D.I. 4 mm');
+            $db_data[] = array('eye' => 'O.I.','ESF' => '+9.20','CIL' => '-1.00','EJE' => '3','ADD' => '+4.50','REF' => 'D.I. 3 mm');
+
+            $col_names = array(
+                'eye' => '',
+                'ESF' => 'ESF.',
+                'CIL' => 'CIL.',
+                'EJE' => 'EJE',
+                'ADD' => 'ADD',
+                'REF' => ''
+            );
+
+            $this->cezpdf->ezTable($db_data, $col_names, 'Graduación registrada el '.date('d').' de Diciembre del '.date('Y'), array('width'=>550));
+            $this->cezpdf->ezText('');
+            $this->cezpdf->ezText('<b>OJO CON LA HORA QUE SALE!!</b> (a mi me sale con 1 hora de retraso)');
+            $this->cezpdf->ezStream(array('Content-Disposition'=>'nama_file.pdf'));
+
+        }
+
         function Prueba()  /*Solo está disñado para la aplicación de Ajax*/
 	{
             if($this->session->userdata('logged_in') == TRUE)

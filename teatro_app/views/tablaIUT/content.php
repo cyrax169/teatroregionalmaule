340746fecce2a62a050aa71a7cb0ea3c205a4569
aKa.NasTy <aKa.NasTy@e7014918-bdd1-11de-ac5a-05e7513655e2>
2009-12-23 20:59:58 +0000

    Iut Terminada

--- teatro_app/views/tablaIUT/content.php
+++ teatro_app/views/tablaIUT/content.php
@@ -21,59 +21,59 @@
             <tbody>
                 <tr>
                     <td><input type="text" name="" value="0.0" readonly /></td>
-                    <td><input type="text" name="" value="<?php echo $a;?>" readonly /></td>
+                    <td><input type="text" name="" value="<?php echo $a + 0.00;?>" readonly /></td>
                     <td><input type="text" name="" value="Exento" readonly /></td>
                     <td><input type="text" name="" value="-.-" readonly /></td>
                     <td><input type="text" name="" value="Exento" readonly /></td>
                 </tr>
                 <tr>
-                    <td><input type="text" name="" value="<?php echo $a;?>" readonly  /></td>
+                    <td><input type="text" name="" value="<?php echo $a + 0.01;?>" readonly  /></td>
                     <td><input type="text" name="" value="<?php echo $b;?>" readonly /></td>
                     <td><input type="text" name="" value="0.05" readonly/></td>
                     <td><input type="text" name="" value="<?php echo $c;?>" readonly/></td>
                     <td><input type="text" name="" value="3%" readonly /></td>
                 </tr>
                 <tr>
-                    <td><input type="text" name="" value="<?php echo $b;?>" readonly /></td>
+                    <td><input type="text" name="" value="<?php echo $b + 0.01;?>" readonly /></td>
                     <td><input type="text" name="" value="<?php echo $d;?>" readonly /></td>
                     <td><input type="text" name="" value="0.10" readonly /></td>
                     <td><input type="text" name="" value="<?php echo $e;?>" readonly /></td>
                     <td><input type="text" name="" value="6%" readonly /></td>
                 </tr>
                 <tr>
-                    <td><input type="text" name="" value="<?php echo $d;?>" readonly /></td>
+                    <td><input type="text" name="" value="<?php echo $d + 0.01;?>" readonly /></td>
                     <td><input type="text" name="" value="<?php echo $f;?>" readonly /></td>
                     <td><input type="text" name="" value="0.15" readonly /></td>
                     <td><input type="text" name="" value="<?php echo $g;?>" readonly /></td>
                     <td><input type="text" name="" value="8%" readonly /></td>
                 </tr>
                 <tr>
-                    <td><input type="text" name="" value="<?php echo $f;?>" readonly /></td>
+                    <td><input type="text" name="" value="<?php echo $f + 0.01;?>" readonly /></td>
                     <td><input type="text" name="" value="<?php echo $h;?>" readonly/></td>
                     <td><input type="text" name="" value="0.25" readonly /></td>
                     <td><input type="text" name="" value="<?php echo $j;?>" readonly /></td>
                     <td><input type="text" name="" value="12%" readonly /></td>
                 </tr>
                 <tr>
-                    <td><input type="text" name="" value="<?php echo $h;?>" readonly/></td>
+                    <td><input type="text" name="" value="<?php echo $h + 0.01;?>" readonly/></td>
                     <td><input type="text" name="" value="<?php echo $i;?>" readonly/></td>
                     <td><input type="text" name="" value="0.32" readonly /></td>
                     <td><input type="text" name="" value="<?php echo $l;?>" readonly /></td>
                     <td><input type="text" name="" value="17%" readonly /></td>
                 </tr>
                 <tr>
-                    <td><input type="text" name="" value="<?php echo $k;?>" readonly /></td>
+                    <td><input type="text" name="" value="<?php echo $k + 0.01;?>" readonly /></td>
                     <td><input type="text" name="" value="<?php echo $m;?>" readonly /></td>
                     <td><input type="text" name="" value="0.37" readonly /></td>
                     <td><input type="text" name="" value="<?php echo $n;?>" readonly /></td>
                     <td><input type="text" name="" value="21%" readonly /></td>
                 </tr>
                 <tr>
-                    <td><input type="text" name="" value="<?php echo $m;?>" readonly /></td>
-                    <td><input type="text" name="" value="Mas" readonly /></td>
+                    <td><input type="text" name="" value="<?php echo $m + 0.01;?>" readonly /></td>
+                    <td><input type="text" name="" value="Más" readonly /></td>
                     <td><input type="text" name="" value="0.4" readonly /></td>
                     <td><input type="text" name="" value="<?php echo $o;?>" readonly /></td>
-                    <td><input type="text" name="" value="Mas de 21%" readonly /></td>
+                    <td><input type="text" name="" value="Más de 21%" readonly /></td>
                 </tr>
             </tbody>
         </table>

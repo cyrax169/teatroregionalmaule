f32a266497bf4f0460eb960a7b4d01cad615d750
claudiadela7 <claudiadela7@e7014918-bdd1-11de-ac5a-05e7513655e2>
2009-12-28 22:00:06 +0000

    muestra rut en liquidacion

--- trunk/teatro_app/views/Liquidacion/content1.php
+++ trunk/teatro_app/views/Liquidacion/content1.php
@@ -1,16 +1,11 @@
-
     <div id="principal">
         <div align="right"><?php echo "Bienvenido ".$username." - ". anchor('usuario/logout','Salir del Sistema');?></div>
         <div class="post" align="center">
             <h2>LIQUIDACIÓN DE SUELDO</h2>
         </div>
         <br><br><br><br>
-      <!-- <form name="frm" method="post" action="<?=base_url()?>index.php/liquidacion_controlador/BuscaRut">-->
-           <form name="frm" method="post" action="<?=base_url()?>index.php/welcome/Muestrarutliquidacion">
-            <div align="center">RUT:
-                <input type="text" name="rut" maxlength="8" /> -
-                <input type="text" name="digito" size="2" maxlength="1" />
-                <br><br>
+        <form name="frm" method="post" action="<?=base_url()?>index.php/liquidacion_controlador/BuscaRut">
+           <div align="center">
                 <select name="mes">
                     <option selected="selected">Mes</option>
                     <option>Enero</option>
@@ -40,17 +35,27 @@
                     <option>2019</option>
                     <option>2020</option>
                 </select>
-            </div>
+                </div>
+             <br>
+            <table id="tabla_simple" cellpadding="0" cellspacing="0" border="1" align="center">
+                <?php $i=0?>
+                <?php foreach($result as $row):?>
+                    <?php if($i<$num): $i++;?>
+                    <tr>
+                        <td><input type="text" name="rut<?php echo $i;?>" value="<?=$row->Rut?>" /></td>
+                        <td><input type="text" name="nombre<?php echo $i;?>" value="<?=$row->Nombre?>" /><input type="checkbox" name="imprime<?php echo $i;?>" value="ON" /></td>
+                    </tr>
+                    <?php endif;?>
+                <?php endforeach;?>
+            </table>
+            <br>
+            
             <br><br><br><br><br>
-           
             <div id="form" align="center">
-                <input class="btn" type="submit" name="Continuar" value="Mostrar rut"/>
+                <input class="btn" type="submit" name="Continuar" value="Mostrar"/>
             </div>
-            <br><br><br><br><br><br><br><br><br><br><br><br>
+            <br>
         </form>
-            <br><br><br>
-    </div>
-
-        <br><br><br><br><br><br><br><br>
+        <br><br><br>
     </div>
 </div>

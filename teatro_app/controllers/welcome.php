870036e0469e2e6b731f6aac5d658e1541d4c031
rodrigopvz <rodrigopvz@e7014918-bdd1-11de-ac5a-05e7513655e2>
2009-12-28 22:47:41 +0000

    se puede crear trabajadores sin carga!

--- teatro_app/controllers/welcome.php
+++ teatro_app/controllers/welcome.php
@@ -793,6 +793,12 @@ class Welcome extends Controller {
                         else
                         {
                             $Cargas = 0;
+                            $nombreCarga='0';
+                            $tipoCarga='0';
+                            $fecha4=date('Ymd');
+                            $rutCarga=0;
+                            $digitoCarga=0;
+                            $this->varios_model->CrearCargas($rut,$nombreCarga,$tipoCarga,$fecha4,$rutCarga,$digitoCarga);
                             $this->varios_model->Crear_Trabajador1($nombres,$rut,$digito2,$fecha1,$direccion,$telefono,$cargo,$tipo_con,$fecha2,$fecha3,$remuneracion,$acaja,$amovilizacion,$acolacion,$afp,$monto_afp,$afc,$tipo_salud,$monto_fonasa,$nombre_isapre,$monto_isapre,$apv_uf,$apv_pesos,$Cargas);
                             $this->load->view('CrearTrabajador/creado',$data);
                         }

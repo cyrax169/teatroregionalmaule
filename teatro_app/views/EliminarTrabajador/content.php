694d13f8599d99c636637ca86357db5260f391dc
claudiadela7 <claudiadela7@e7014918-bdd1-11de-ac5a-05e7513655e2>
2009-12-29 17:35:18 +0000

    muestra rut a eliminar

--- teatro_app/views/EliminarTrabajador/content.php
+++ teatro_app/views/EliminarTrabajador/content.php
@@ -4,14 +4,30 @@
             <h2>ELIMINAR TRABAJADOR</h2>
         </div>
         <br><br><br><br><br><br><br>
-        <form name="frm" method="post" action="<?=base_url()?>index.php/welcome/EliminaTrabajador">
-            <div align="center">RUT:
-                <input type="text" name="rut" value="" maxlength="8" /> -
+       <form name="frm" method="post" action="<?=base_url()?>index.php/welcome/EliminaTrabajador">
+
+       <!--          <input type="text" name="rut" value="" maxlength="8" /> -
                 <input type="text" name="digito" size="2" maxlength="1" />
-            </div>
+            </div>-->
+            <table id="tabla_simple" cellpadding="0" cellspacing="0" border="1" align="center">
+                <?php $i=0?>
+                <?php foreach($result as $row):?>
+                    <?php if($i<$num): $i++;?>
+                        <?php if($row->Estado ==1):?>
+                    <tr>
+
+                        <td><input type="text" name="rut<?php echo $i;?>" value="<?=$row->Rut?>" /></td>
+                        <td><input type="text" name="nombre<?php echo $i;?>" value="<?=$row->Nombre?>" />
+                        <input type="radio" name="imprime" value="<?php echo $i;?>" /></td>
+
+                    </tr>
+                        <?php endif;?>
+                    <?php endif;?>
+                <?php endforeach;?>
+            </table>
             <br><br><br><br><br>
             <div id="form" align="center">
-                <input class="btn" type="submit" name="Continuar" value="Buscar"/>
+                <input class="btn" type="submit" name="Continuar" value="Eliminar"/>
             </div>
             <br><br><br><br><br><br><br><br><br><br><br><br>
         </form>
--- teatro_app/views/EliminarTrabajador/eliminado.php
+++ teatro_app/views/EliminarTrabajador/eliminado.php
@@ -7,7 +7,7 @@
         <h2 align="center">Trabajador eliminado con éxito</h2>
         <br><br><br><br><br><br>
         <div id="form" align="center">
-            <form name="frm" method="post" action="../welcome/EliminarTrabajador">
+            <form name="frm" method="post" action="../welcome/MuestrarutEliminar">
                 <input class="btn" type="submit" name="Continuar" value="Continuar"/>
             </form>
         </div>

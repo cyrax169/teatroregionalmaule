15d54dee7cfee0d628d7cf13371a011ab7949e73
claudiadela7 <claudiadela7@e7014918-bdd1-11de-ac5a-05e7513655e2>
2009-12-29 21:36:24 +0000

    modificar usuario seguro

--- trunk/teatro_app/views/supervisor/content.php
+++ trunk/teatro_app/views/supervisor/content.php
@@ -5,10 +5,21 @@
         </div>
         <br><br><br><br><br><br><br>
         <form name="frm" method="post" action="<?=base_url()?>index.php/welcome/Modifica_supervisor">
-            <div align="center">RUT:
-                <input type="text" name="RUT" value="" maxlength="8" /> -
-                <input type="text" name="DIGITO" size="2" maxlength="1" />
-            </div>
+            <div align="center">
+            <table border="1">
+                     <tr>
+                        <th>RUT:</th>
+                        <th><input type="text" name="RUT" size="13" value="" maxlength="8" /> -
+                        <input type="text" name="DIGITO" size="1" maxlength="1" /></th>
+                    </tr>
+                     <tr>
+                        <td>Password:</td>
+                        <td><input type="password" name="password" /></td>
+                    </tr>
+
+
+            </table>
+</div>
             <br><br><br><br><br>
             <div id="form" align="center">
                 <input class="btn" type="submit" name="Continuar" value="Buscar"/>

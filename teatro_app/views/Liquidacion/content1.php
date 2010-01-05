d4c6d43a72d1aa75b2f4014a50bd3d46d6c6a2c8
aKa.NasTy <aKa.NasTy@e7014918-bdd1-11de-ac5a-05e7513655e2>
2010-01-05 22:18:56 +0000

    casi casi casi lista

--- teatro_app/views/Liquidacion/content1.php
+++ teatro_app/views/Liquidacion/content1.php
@@ -7,7 +7,7 @@
         <form name="frm" method="post" action="<?=base_url()?>index.php/liquidacion_controlador/BuscaRut">
            <div align="center">
                <select name="mes">
-                   <option selected><?echo $mes?></option>
+                   <option selected><?echo $mes?></option>)
                     <option>Enero</option>
                     <option>Febrero</option>
                     <option>Marzo</option>
@@ -22,19 +22,13 @@
                     <option>Diciembre</option>
                 </select>
                 <select name="anio">
-                    <option selected="selected" >2009</option>
-                    <option>2010</option>
-                    <option>2011</option>
-                    <option>2012</option>
-                    <option>2013</option>
-                    <option>2014</option>
-                    <option>2015</option>
-                    <option>2016</option>
-                    <option>2017</option>
-                    <option>2018</option>
-                    <option>2019</option>
-                    <option>2020</option>
-                </select>
+                    <option selected><?echo $anio?></option>
+                     <? for($i=2008;$i<2030;$i++):?>
+                        <? if ($anio != $i):?>
+                         <option> <? echo $i;?> </option>
+                        <? endif;?>
+                    <? endfor; ?>
+                </select>            
                 </div>
              <br>
             <table id="tabla_simple" cellpadding="0" cellspacing="0" border="1" align="center">

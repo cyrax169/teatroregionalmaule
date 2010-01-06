416047c23745a490805ef89e73554408cd4afe9d
rodrigopvz <rodrigopvz@e7014918-bdd1-11de-ac5a-05e7513655e2>
2010-01-06 18:56:23 +0000

    JS!

--- js/function.js
+++ js/function.js
@@ -97,11 +97,13 @@ function hiddenTextBox()
 }
 function showTextBox_trabajador()
 {
+    $("#fecha_inicio").show();
     $("#fecha_termino").show();
     $("#Afc").hide();
 }
 function hiddenTextBox_trabajador()
 {
+    $("#fecha_inicio").show();
     $("#fecha_termino").hide();
     $("#Afc").show();
 }

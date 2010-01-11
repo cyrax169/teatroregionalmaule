5b18ba322b624a5b60c481b11b591786d769d8ff
claudiadela7 <claudiadela7@e7014918-bdd1-11de-ac5a-05e7513655e2>
2010-01-11 20:42:50 +0000

    nuevos cambios planilla

--- trunk/teatro_app/views/planilla/content.php
+++ trunk/teatro_app/views/planilla/content.php
@@ -41,8 +41,8 @@
                                     <td>NOMBRE ISAPRE</td>
                                     <td>FONASA 6.4</td>
                                     <td>CAJA</td>
-                                    <td>AVP</td>
-                                    <td>TOTAL DESCUENTOS LEGALES + APV</td>
+                                    <td>APV</td>
+                                    <td>TOTAL DESCUENTOS LEGALES</td>
                                     <td>BASE IMPUESTO</td>
                                     <td>IPM. UNI</td>
                                     <td>SEGUROS/CREDITOS</td>
@@ -59,74 +59,74 @@
                                
                                     <?php if($i<$num): $i++;?>
 
-                                    <td><input type="text" name="NOMBRE<?php echo $i;?>" value="<?=$row->Nombre?>" /></td>
-                                    <td><input type="text" name="RUT<?php echo $i;?>" value="<?=$row->Rut?>-<?=$row->Digito?>" /></td>
-                                    <td><input type="text" name="RENTABRUTA<?php echo $i;?>" value="<?=$row->RentaBruta?>" /></td>
-                                    <td><input type="text" name="DIASTRABAJADOS<?php echo $i;?>" value="<?=$row->DiasTrabajados?>" /></td>
-                                    <td><input type="text" name="HORASEXTRAS<?php echo $i;?>" value="<?=$row->HorasExtras?>" /></td>
-                                    <td><input type="text" name="AGUINALDO<?php echo $i;?>" value="<?=$row->OtrosBonos?>" /></td>
-                                    <td><input type="text" name="RENTAIMPONIBLE<?php echo $i;?>" value="<?=$row->RentaImponible?>" /></td>
-                                    <td><input type="text" name="ACAJAOTROS<?php echo $i;?>" value="<?=$row->AcajaOtro?>" /></td>
-                                    <td><input type="text" name="NUMCARGA<?php echo $i;?>" value="<?=$row->NumCargas?>" /></td>
-                                    <td><input type="text" name="ASIGNACIONFAMILIAR<?php echo $i;?>" value="<?=$row->AsignacionFamiliar?>" /></td>
-                                    <td><input type="text" name="TOTALHABERES<?php echo $i;?>" value="<?=$row->TotalHaberes?>" /></td>
+                                    <td><input type="text"readonly="readonly" name="NOMBRE<?php echo $i;?>" value="<?=$row->Nombre?>" /></td>
+                                    <td><input type="text"readonly="readonly" name="RUT<?php echo $i;?>" value="<?=$row->Rut?>-<?=$row->Digito?>" /></td>
+                                    <td><input type="text"readonly="readonly" name="RENTABRUTA<?php echo $i;?>" value="<?=$row->RentaBruta?>" /></td>
+                                    <td><input type="text"readonly="readonly" name="DIASTRABAJADOS<?php echo $i;?>" value="<?=$row->DiasTrabajados?>" /></td>
+                                    <td><input type="text"readonly="readonly" name="HORASEXTRAS<?php echo $i;?>" value="<?=$row->HorasExtras?>" /></td>
+                                    <td><input type="text"readonly="readonly" name="AGUINALDO<?php echo $i;?>" value="<?=$row->OtrosBonos?>" /></td>
+                                    <td><input type="text"readonly="readonly" name="RENTAIMPONIBLE<?php echo $i;?>" value="<?=$row->RentaImponible?>" /></td>
+                                    <td><input type="text"readonly="readonly" name="ACAJAOTROS<?php echo $i;?>" value="<?=$row->AcajaOtro?>" /></td>
+                                    <td><input type="text"readonly="readonly" name="NUMCARGA<?php echo $i;?>" value="<?=$row->NumCargas?>" /></td>
+                                    <td><input type="text"readonly="readonly" name="ASIGNACIONFAMILIAR<?php echo $i;?>" value="<?=$row->AsignacionFamiliar?>" /></td>
+                                    <td><input type="text"readonly="readonly" name="TOTALHABERES<?php echo $i;?>" value="<?=$row->TotalHaberes?>" /></td>
                                    <?php $Nombreafp=$row->NombreAfp;?>
                                     <?php if($Nombreafp=='Capital'):?>
-                                    <td><input type="text" name="PROVIDA<?php echo $i;?>" value="0" /></td>
-                                    <td><input type="text" name="HABITAT<?php echo $i;?>"value="0"</td> 
-                                    <td><input type="text" name="CAPITAL<?php echo $i;?>" value="<?=$row->MontoAfp?>" /></td>
-                                    <td><input type="text" name="CUPRUM<?php echo $i;?>" value="0" /></td>
-                                    <td><input type="text" name="PLAN VITAL<?php echo $i;?>" value="0" /></td>
+                                    <td><input type="text"readonly="readonly" name="PROVIDA<?php echo $i;?>" value="0" /></td>
+                                    <td><input type="text"readonly="readonly" name="HABITAT<?php echo $i;?>"value="0"</td>
+                                    <td><input type="text"readonly="readonly" name="CAPITAL<?php echo $i;?>" value="<?=$row->MontoAfp?>" /></td>
+                                    <td><input type="text"readonly="readonly" name="CUPRUM<?php echo $i;?>" value="0" /></td>
+                                    <td><input type="text"readonly="readonly" name="PLAN VITAL<?php echo $i;?>" value="0" /></td>
                                     <?php endif;?>
                                      <?php if($Nombreafp=='Habitat'):?>
-                                    <td><input type="text" name="PROVIDA<?php echo $i;?>" value="0" /></td>
-                                    <td><input type="text" name="HABITAT<?php echo $i;?>"value="<?=$row->MontoAfp?>"</td> <!--Nosé si va-->
-                                    <td><input type="text" name="CAPITAL<?php echo $i;?>" value="0" /></td>
-                                    <td><input type="text" name="CUPRUM<?php echo $i;?>" value="0" /></td>
-                                    <td><input type="text" name="PLAN VITAL<?php echo $i;?>" value="0" /></td>
+                                    <td><input type="text"readonly="readonly" name="PROVIDA<?php echo $i;?>" value="0" /></td>
+                                    <td><input type="text"readonly="readonly" name="HABITAT<?php echo $i;?>"value="<?=$row->MontoAfp?>"</td> <!--Nosé si va-->
+                                    <td><input type="text"readonly="readonly" name="CAPITAL<?php echo $i;?>" value="0" /></td>
+                                    <td><input type="text"readonly="readonly" name="CUPRUM<?php echo $i;?>" value="0" /></td>
+                                    <td><input type="text"readonly="readonly" name="PLAN VITAL<?php echo $i;?>" value="0" /></td>
                                     <?php endif;?>
                                      <?php if($Nombreafp=='Cuprum'):?>
-                                    <td><input type="text" name="PROVIDA<?php echo $i;?>" value="0" /></td>
-                                    <td><input type="text" name="HABITAT<?php echo $i;?>"value="0"</td> <!--Nosé si va-->
-                                    <td><input type="text" name="CAPITAL<?php echo $i;?>" value="0" /></td>
-                                    <td><input type="text" name="CUPRUM<?php echo $i;?>" value="<?=$row->MontoAfp?>" /></td>
-                                    <td><input type="text" name="PLAN VITAL<?php echo $i;?>" value="0" /></td>
+                                    <td><input type="text"readonly="readonly" name="PROVIDA<?php echo $i;?>" value="0" /></td>
+                                    <td><input type="text"readonly="readonly" name="HABITAT<?php echo $i;?>"value="0"</td> <!--Nosé si va-->
+                                    <td><input type="text"readonly="readonly" name="CAPITAL<?php echo $i;?>" value="0" /></td>
+                                    <td><input type="text"readonly="readonly" name="CUPRUM<?php echo $i;?>" value="<?=$row->MontoAfp?>" /></td>
+                                    <td><input type="text"readonly="readonly" name="PLAN VITAL<?php echo $i;?>" value="0" /></td>
                                     <?php endif;?>
                                      <?php if($Nombreafp=='Plan Vital'):?>
-                                    <td><input type="text" name="PROVIDA<?php echo $i;?>" value="0" /></td>
-                                    <td><input type="text" name="HABITAT<?php echo $i;?>"value="0"</td> <!--Nosé si va-->
-                                    <td><input type="text" name="CAPITAL<?php echo $i;?>" value="0" /></td>
-                                    <td><input type="text" name="CUPRUM<?php echo $i;?>" value="0" /></td>
-                                    <td><input type="text" name="PLAN VITAL<?php echo $i;?>" value="<?=$row->MontoAfp?>" /></td>
+                                    <td><input type="text"readonly="readonly" name="PROVIDA<?php echo $i;?>" value="0" /></td>
+                                    <td><input type="text"readonly="readonly" name="HABITAT<?php echo $i;?>"value="0"</td> <!--Nosé si va-->
+                                    <td><input type="text"readonly="readonly" name="CAPITAL<?php echo $i;?>" value="0" /></td>
+                                    <td><input type="text"readonly="readonly" name="CUPRUM<?php echo $i;?>" value="0" /></td>
+                                    <td><input type="text"readonly="readonly" name="PLAN VITAL<?php echo $i;?>" value="<?=$row->MontoAfp?>" /></td>
                                     <?php endif;?>
                                     <?php if($Nombreafp=='Provida'):?>
-                                    <td><input type="text" name="PROVIDA<?php echo $i;?>" value="<?=$row->MontoAfp?>" /></td>
-                                    <td><input type="text" name="HABITAT<?php echo $i;?>"value="0"</td> <!--Nosé si va-->
-                                    <td><input type="text" name="CAPITAL<?php echo $i;?>" value="0" /></td>
-                                    <td><input type="text" name="CUPRUM<?php echo $i;?>" value="0" /></td>
-                                    <td><input type="text" name="PLAN VITAL<?php echo $i;?>" value="0" /></td>
+                                    <td><input type="text"readonly="readonly" name="PROVIDA<?php echo $i;?>" value="<?=$row->MontoAfp?>" /></td>
+                                    <td><input type="text"readonly="readonly" name="HABITAT<?php echo $i;?>"value="0"</td> <!--Nosé si va-->
+                                    <td><input type="text"readonly="readonly" name="CAPITAL<?php echo $i;?>" value="0" /></td>
+                                    <td><input type="text"readonly="readonly" name="CUPRUM<?php echo $i;?>" value="0" /></td>
+                                    <td><input type="text"readonly="readonly" name="PLAN VITAL<?php echo $i;?>" value="0" /></td>
                                     <?php endif;?>
-                                    <td><input type="text" name="AFC<?php echo $i;?>" value="<?=$row->Afc?>" /></td>
+                                    <td><input type="text"readonly="readonly" name="AFC<?php echo $i;?>" value="<?=$row->Afc?>" /></td>
                                   
-                                    <td><input type="text" name="ISAPREVARIOS<?php echo $i;?>" value="<?=$row->MontoIsapre?>" /></td>
+                                    <td><input type="text"readonly="readonly" name="ISAPREVARIOS<?php echo $i;?>" value="<?=$row->MontoIsapre?>" /></td>
                                   
-                                    <td><input type="text" name="ISAPREADICIONAL<?php echo $i;?>" value="<?=$row->IsapreAdicional?>" /></td>
+                                    <td><input type="text"readonly="readonly" name="ISAPREADICIONAL<?php echo $i;?>" value="<?=$row->IsapreAdicional?>" /></td>
                                  
-                                    <td><input type="text" name="NOMBREISAPRE<?php echo $i;?>" value="<?=$row->NombreIsapre?>"
+                                    <td><input type="text"readonly="readonly" name="NOMBREISAPRE<?php echo $i;?>" value="<?=$row->NombreIsapre?>"
                                
-                                    <td><input type="text" name="FONASA<?php echo $i;?>" value="<?=$row->Fonasa?>" /></td>
-                                    <td><input type="text" name="LOSANDES<?php echo $i;?>" value="<?=$row->LosAndes?>" /></td>
-                                    <td><input type="text" name="APV<?php echo $i;?>" value="<?=$row->Apv?>" /></td>
-                                    <td><input type="text" name="TOTALDESCUENTOSLEGALES<?php echo $i;?>" value="<?=$row->TotalDescuentosLegales?>" /></td>
-                                    <td><input type="text" name="BaseImpueto<?php echo $i;?>" value="<?=$row->BaseImpuesto?>" /></td>
-                                    <td><input type="text" name="IPM.UNI<?php echo $i;?>" value="<?=$row->IpmUni?>" /></td>
-                                    <td><input type="text" name="CREDITOS<?php echo $i;?>" value="<?=$row->Prestamos?>" /></td>
-                                    <td><input type="text" name="ANTICIPOS<?php echo $i;?>" value="<?=$row->AnticiposOtros?>" /></td>
-                                    <td><input type="text" name="TOTALDESCUENTOSADICIONALES<?php echo $i;?>" value="<?=$row->TotalDescuentosAdicionaes?>" /></td>
-                                    <td><input type="text" name="TOTALLIQUIDO<?php echo $i;?>" value="<?=$row->TotalLiquido?>" /></td>
-                                    <td><input type="text" name="AFC2<?php echo $i;?>" value="<?=$row->Afctrabajador?>" /></td>
-                                    <td><input type="text" name="AFC3<?php echo $i;?>" value="<?=$row->Afctrabajador1?>" /></td>
-                                    <td><input type="text" name="APORTE<?php echo $i;?>" value="<?=$row->Aporte?>" /></td>
+                                    <td><input type="text"readonly="readonly" name="FONASA<?php echo $i;?>" value="<?=$row->Fonasa?>" /></td>
+                                    <td><input type="text"readonly="readonly" name="LOSANDES<?php echo $i;?>" value="<?=$row->LosAndes?>" /></td>
+                                    <td><input type="text"readonly="readonly" name="APV<?php echo $i;?>" value="<?=$row->Apv?>" /></td>
+                                    <td><input type="text"readonly="readonly" name="TOTALDESCUENTOSLEGALES<?php echo $i;?>" value="<?=$row->TotalDescuentosLegales?>" /></td>
+                                    <td><input type="text"readonly="readonly" name="BaseImpueto<?php echo $i;?>" value="<?=$row->BaseImpuesto?>" /></td>
+                                    <td><input type="text"readonly="readonly" name="IPM.UNI<?php echo $i;?>" value="<?=$row->IpmUni?>" /></td>
+                                    <td><input type="text"readonly="readonly" name="CREDITOS<?php echo $i;?>" value="<?=$row->Prestamos?>" /></td>
+                                    <td><input type="text"readonly="readonly" name="ANTICIPOS<?php echo $i;?>" value="<?=$row->AnticiposOtros?>" /></td>
+                                    <td><input type="text"readonly="readonly" name="TOTALDESCUENTOSADICIONALES<?php echo $i;?>" value="<?=$row->TotalDescuentosAdicionaes?>" /></td>
+                                    <td><input type="text"readonly="readonly" name="TOTALLIQUIDO<?php echo $i;?>" value="<?=$row->TotalLiquido?>" /></td>
+                                    <td><input type="text"readonly="readonly" name="AFC2<?php echo $i;?>" value="<?=$row->Afctrabajador?>" /></td>
+                                    <td><input type="text"readonly="readonly" name="AFC3<?php echo $i;?>" value="<?=$row->Afctrabajador1?>" /></td>
+                                    <td><input type="text"readonly="readonly" name="APORTE<?php echo $i;?>" value="<?=$row->Aporte?>" /></td>
                                    
                                 </tr>
                               

928cb01077dce2af1a3479f237a71f5e1691d84d
claudiadela7 <claudiadela7@e7014918-bdd1-11de-ac5a-05e7513655e2>
2010-01-12 07:06:54 +0000

    afc planilla

--- trunk/teatro_app/controllers/planilla_controlador.php
+++ trunk/teatro_app/controllers/planilla_controlador.php
@@ -183,6 +183,19 @@
                             }
                             else
                              $Afc = 0;
+                                       $TipoContrato=$row6->TipoContrato;
+                            if($TipoContrato=='Fijo'){
+                                 if($TotalImponible>$TopeAfc)
+                                    $AfcEmp1=$TopeAfc*3/100;
+                                 else
+                                    $AfcEmp1=$TotalImponible*3/100;
+                            }
+                            else{
+                                if($TotalImponible>$TopeAfc)
+                                    $AfcEmp=$TopeAfc*2.4/100;
+                                else
+                                    $AfcEmp=$TotalImponible*2.4/100;
+                            }
                             $TopeImponible = 60*$UF;
                             if ($TotalImponible > $TopeImponible){
                                 $BonoNoImponible = ($TotalImponible - $TopeImponible);
@@ -222,19 +235,7 @@
                             $nombreafp=$row6->NombreAfp;
 
                             $horas=$row6->HorasExtras;
-                                    $TipoContrato=$row6->TipoContrato;
-                            if($TipoContrato=='Fijo'){
-                                 if($TotalImponible>$TopeAfc)
-                                    $AfcEmp1=$TopeAfc*3/100;
-                                 else
-                                    $AfcEmp1=$TotalImponible*3/100;
-                            }
-                            else{
-                                if($TotalImponible>$TopeAfc)
-                                    $AfcEmp=$TopeAfc*2.4/100;
-                                else
-                                    $AfcEmp=$TotalImponible*2.4/100;
-                            }
+                          
                             $aporte=$TotalImponible*0.95/100;
                             $remuneracion=$row6->Salario;
                             $var4 = $row6->Acaja;

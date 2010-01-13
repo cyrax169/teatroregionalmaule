6375715ad591c0ca2c184f22a8639d4420545e4b
rodrigopvz <rodrigopvz@e7014918-bdd1-11de-ac5a-05e7513655e2>
2010-01-13 17:10:20 +0000

    usuarios del sistema

--- trunk/teatro_app/views/Modificar_Admin/content.php
+++ trunk/teatro_app/views/Modificar_Admin/content.php
@@ -4,11 +4,22 @@
             <h2>MODIFICAR ADMINISTRADOR</h2>
         </div>
         <br><br><br><br><br><br><br>
-        <form name="frm" method="post" action="<?=base_url()?>index.php/welcome/Modifica_Admin">
-            <div align="center">RUT:
-                <input type="text" name="RUT" value="" maxlength="8" /> -
-                <input type="text" name="DIGITO" size="2" maxlength="1" />
-            </div>
+        <form name="frm" method="post" action="<?=base_url()?>index.php/welcome/Modifica_admin">
+            <div align="center">
+            <table border="1">
+                     <tr>
+                        <th>RUT:</th>
+                        <th><input type="text" name="RUT" size="13" value="" maxlength="8" /> -
+                        <input type="text" name="DIGITO" size="1" maxlength="1" /></th>
+                    </tr>
+                     <tr>
+                        <th>PASSWORD:</th>
+                        <th><input type="password" name="password" /></th>
+                    </tr>
+
+
+            </table>
+</div>
             <br><br><br><br><br>
             <div id="form" align="center">
                 <input class="btn" type="submit" name="Continuar" value="Buscar"/>
